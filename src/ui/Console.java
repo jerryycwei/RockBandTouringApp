7c056ae680d8f936bf7954e4948c116446df165a
Ivelin Bratanov <ivelin.bratanov@mail.mcgill.ca>
2014-11-10 17:36:25 -0500

    Fix merge conflicts and update with latest version of console.

--- src/ui/Console.java
+++ src/ui/Console.java
@@ -1,6 +1,8 @@
 package ui;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.TextArea;
 
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -9,14 +11,16 @@ public class Console extends JPanel {
 	private JTextArea textArea;
 	
 	public Console() {
-		this.setBounds(0, 613, 915, 80);
-		textArea = new JTextArea();
-		textArea.setBounds(0, 613, 915, 80);
+		this.setBounds(0, 602, 1100, 80); //update to reflect infoLabel
+		textArea = new JTextArea(20, 100);
+		textArea.setSize( new Dimension(1001, 80) );
+		//new TextArea("Coordinates clicked:\nShape 1:\n", 6, 40, ta.SCROLLBARS_VERTICAL_ONLY); 
+		//textArea.setBounds(0, 602, 1100, 80);
 		textArea.setEditable(false);
 		textArea.setVisible(true);
-		textArea.setBackground(new Color(205,127,50));
+		textArea.setBackground(new Color(51,255,153));
 		this.add(textArea);
-		this.setBackground(new Color(100, 100, 100));
+		this.setBackground(new Color(51,255,153));
 	}
 	public void setText(String text) {
 		textArea.setText(text);
