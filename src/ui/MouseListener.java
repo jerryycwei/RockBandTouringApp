76be537981ccbce0f5644a8303764d1c85a82c11
nhat-quangdao <nhat.quang.d@gmail.com>
2014-11-10 20:26:46 -0500

    implemented undo and redo

--- src/ui/MouseListener.java
+++ src/ui/MouseListener.java
@@ -3,6 +3,8 @@ package ui;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import metamodel.City;
 
@@ -14,14 +16,23 @@ public class MouseListener extends MouseAdapter implements MouseMotionListener{
 	private static final int CLICK_RADIUS = 10;
 	private static final int HOVER_RADIUS = 10;
 	
+	private HashMap<MouseEvent, City> mouseMap;
+	private ArrayList<MouseEvent> mouseUndoStack = new ArrayList<MouseEvent>();
+	private ArrayList<MouseEvent> mouseRedoStack = new ArrayList<MouseEvent>();
+	private ArrayList<City> undoCity = new ArrayList<City>();
+	
 	public MouseListener(Main parent, ImagePanel imgPanel, Console console) {
 		this.parent = parent;
 		this.imgPanel = imgPanel;
 		this.console = console;
+		mouseUndoStack = new ArrayList<MouseEvent>();
+		mouseRedoStack = new ArrayList<MouseEvent>();
+		undoCity = new ArrayList<City>();
 	}
 	
 	
 	public void mouseClicked(MouseEvent e) {
+		//pushMouseEvent(mouseUndoStack, e);
 		int x =e.getX(); // the specific x coordinate of the point clicked is stored in a place in the array
         int y =e.getY(); // the specific y coordinate of the point clicked is stored in a place in the array
         console.setText("Xclicked: " + x + "\nYclicked: " + y + "\n");
@@ -30,6 +41,9 @@ public class MouseListener extends MouseAdapter implements MouseMotionListener{
         	if (x < (city.getCircle().getPositionX() + CLICK_RADIUS) && x > (city.getCircle().getPositionX() - CLICK_RADIUS) &&
         			y < (city.getCircle().getPositionY() + CLICK_RADIUS) && y > (city.getCircle().getPositionY() - CLICK_RADIUS)) {
         		console.setText("added city " + city.getName());
+        		//city.setSelected(true);
+        		//mouseMap.put(e, city);
+        		//repaint();
         	}
         }
 	}
@@ -44,7 +58,7 @@ public class MouseListener extends MouseAdapter implements MouseMotionListener{
 	        int y =e.getY(); // the specific y coordinate of the point clicked is stored in a place in the array
 	        for (City city : parent.getCities()) {
 	        	if (x < (city.getCircle().getPositionX() + HOVER_RADIUS) && x > (city.getCircle().getPositionX() - HOVER_RADIUS) &&
-	        			y < (city.getCircle().getPositionY() + HOVER_RADIUS) && y > (city.getCircle().getPositionY() - HOVER_RADIUS)) {
+	        			y < (city.getCircle().getPositionY() + HOVER_RADIUS) && y > (city.getCircle().getPositionY() - HOVER_RADIUS)) {	     
 	        		parent.getInfoLabel().getInfo().setText(city.getName() +"\n" + city.getAccomodation(0).getName());
 	        	}
 	        }
@@ -53,6 +67,43 @@ public class MouseListener extends MouseAdapter implements MouseMotionListener{
 	
 	public void mouseExited(MouseEvent e) {
 		insideMap = false;
+	}
+	
+	public void undo(){
+		MouseEvent mouseUndoPopped = mouseUndoStack.get(mouseRedoStack.size() - 1);
+		pushMouseEvent(mouseRedoStack, mouseUndoPopped);
+		City cityToBeUndo = mouseMap.get(mouseUndoPopped);
+		cityToBeUndo.setSelected(false);
+		mouseMap.remove(mouseUndoPopped);
+		pushCityStack(undoCity, cityToBeUndo);
+		repaint();
+	}
+	
+	public void redo(){
+		MouseEvent mouseRedoPopped = mouseRedoStack.get(mouseRedoStack.size() - 1);
+		pushMouseEvent(mouseUndoStack, mouseRedoPopped);
+		City redoCity = undoCity.remove(undoCity.size() - 1);
+		redoCity.setSelected(true);
+		mouseMap.put(mouseRedoPopped, redoCity);	
+		repaint();
+	}
+	
+	private void repaint() {
+		imgPanel.repaint();
+	}
+	
+	private void pushMouseEvent(ArrayList<MouseEvent> list, MouseEvent element) {
+		list.add(element);
+		if (list.size() > 5) {
+			list.remove(0);
+		}
+	}
+	
+	private void pushCityStack(ArrayList<City> list, City element) {
+		list.add(element);
+		if (list.size() > 5) {
+			list.remove(0);
+		}
 	}
 	
 	
